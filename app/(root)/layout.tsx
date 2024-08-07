eb5c5a2c64209eefb558b930195df2b5b42737fe
David Ayres <davidrayres@hotmail.com>
2024-08-07 18:51:56 -0400

    sidebars framework finished

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 // import MobileNav from '@/components/MobileNav'
-// import Sidebar from '@/components/Sidebar'
-// import {getLoggedInUser} from '@/lib/actions/user.actions'
+import MobileNav from '@/components/MobileNav'
+import Sidebar from '@/components/Sidebar'
+import {getLoggedInUser} from '@/lib/actions/user.actions'
 import Image from 'next/image'
 import {redirect} from 'next/navigation'
 
@@ -9,18 +10,21 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggedIn = {firstName: 'David', lastName: 'Ayres'}
   // const loggedIn = await getLoggedInUser()
 
   // if (!loggedIn) redirect('/sign-in')
 
   return (
     <main className='flex h-screen w-full font-inter'>
-      {/* <Sidebar user={loggedIn} /> */}
+      <Sidebar user={loggedIn} />
 
       <div className='flex size-full flex-col'>
         <div className='root-layout'>
           <Image src='/icons/logo.svg' width={30} height={30} alt='logo' />
-          <div>{/* <MobileNav user={loggedIn} /> */}</div>
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
         </div>
         {children}
       </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,42 @@
-export default function HomePage() {
+import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+// import {getAccount, getAccounts} from '@/lib/actions/bank.actions'
+import {getLoggedInUser} from '@/lib/actions/user.actions'
+
+const Home = async ({searchParams: {id, page}}: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1
+  const loggedIn = {
+    firstName: 'David',
+    lastName: 'Ayres',
+    email: 'draclt@icloud.com',
+  }
+  // const loggedIn = await getLoggedInUser()
+  // const accounts = await getAccounts({userId: loggedIn.$id})
+
+  // if (!accounts) return
+
+  // const accountsData = accounts?.data
+  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId
+
+  // const account = await getAccount({appwriteItemId})
+
   return (
-    <div>HomePage</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting' title='Welcome' user={loggedIn?.firstName || 'Guest'} subtext='Access and manage your account and transactions efficiently.' />
+
+          <TotalBalanceBox accounts={[]} totalBanks={3} totalCurrentBalance={10000} />
+        </header>
+
+        {/* <RecentTransactions accounts={accountsData} transactions={account?.transactions} appwriteItemId={appwriteItemId} page={currentPage} /> */}
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.5}, {currentBalance: 543.23}]} />
+    </section>
   )
 }
+
+export default Home
